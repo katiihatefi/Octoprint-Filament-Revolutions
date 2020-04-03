721f9e9cc58a9f2041827a91e18e8362a8cc39d4
katihh <63033694+katihh@users.noreply.github.com>
2020-04-03 11:51:09 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ plugin_author = "Jérôme Wiedemann"
 plugin_author_email = ""
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/RomRider/Octoprint-Filament-Revolutions"
+plugin_url = "https://github.com/katihh/Octoprint-Filament-Revolutions"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
